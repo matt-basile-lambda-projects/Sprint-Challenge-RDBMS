7fb6bfc62af306d54ea2fee6983657c330a6c4f8
Matt Basile <matthewbasile2@gmail.com>
2019-02-22 12:34:32 -0500

    Added extra Crud Calls

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const db = knex(knexConfig.development);
 const server = express();
 server.use(express.json());
 
+
+
+server.get('/projects/', async (req, res) =>{
+    try {
+    const projects = await db('projects')
+     res.status(200).json(projects)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 server.get('/projects/:id', async (req, res) =>{
     try {
     const project = await db('projects').where({id: req.params.id});
-    const actions = await db('actions', 'projects.id', '=', 'actions.project_id')
+    const actions = await db("actions").where({project_id: req.params.id});
     const answer = {
         ...project[0],
         actions
@@ -20,14 +30,6 @@ server.get('/projects/:id', async (req, res) =>{
         res.status(500).json(error)
     }
 })
-
-
-
-
-
-
-
-
 server.post('/projects', async (req, res) => {
     try{
       const [id] = await db('projects').insert(req.body);
@@ -38,6 +40,22 @@ server.post('/projects', async (req, res) => {
      res.status(500).json(error);
     }
 });
+server.get('/actions', async (req, res) =>{
+    try {
+    const actions = await db('actions');
+    res.status(200).json(actions)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+server.get('/actions/:id', async (req, res) =>{
+    try {
+    const action = await db('actions').where({id: req.params.id});
+    res.status(200).json(action)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 server.post('/actions', async (req, res) => {
     try{
       const [id] = await db('actions').insert(req.body);
@@ -48,6 +66,7 @@ server.post('/actions', async (req, res) => {
      res.status(500).json(error);
     }
 });
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, ()=> console.log(`\n Running on ${port}\n`))
